2ed7dd69b9f5088ea3d23512ae7be59ced76ce1e
RINS <73406219+RINS-I@users.noreply.github.com>
2024-04-23 22:48:37 +0100

    Time: 91 ms (57.82%) | Memory: 22.6 MB (95.47%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word)
+    {
+        vector<pair<int, int> > hash(26, std::make_pair(0, 0));
+        int ans = 0;
+
+        for (size_t i = 0; i < word.length(); i++)
+        {
+            if (isupper(word[i]) and hash[word[i] - 'A'].second >= 0)
+                hash[word[i] - 'A'].first += 1;
+            else if (isupper(word[i]) and hash[word[i] - 'A'].second == 0)
+                hash[word[i] - 'A'].second = -1;
+            else if (islower(word[i]) and hash[word[i] - 'a'].first == 0)
+                hash[word[i] - 'a'].second += 1;
+            else if (islower(word[i]) and hash[word[i] - 'a'].first >= 0)
+                hash[word[i] - 'a'].second = -1;
+        }
+
+        for (size_t i = 0; i < hash.size(); i++)
+        {
+            cout << hash[i].first << " " << hash[i].second <<"\n";
+            if (hash[i].first > 0 and hash[i].second > 0)
+                ans++;
+        }
+        return ans;
+    }
+};
