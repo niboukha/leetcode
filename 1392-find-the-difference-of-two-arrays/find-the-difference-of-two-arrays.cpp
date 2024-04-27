f627a23d96a1b8c36acd914a8570fd0317dc2e86
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-04-28 00:10:33 +0100

    Time: 39 ms (62.75%) | Memory: 34.1 MB (87.15%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -6,15 +6,10 @@ public:
         vector<vector<int> > ans(2);
 
         for(int i = 0; i < nums1.size(); i++)
-        {
             hash[nums1[i]].first += 1;
-            hash[nums1[i]].second += 0;
-        }
         for(int i = 0; i < nums2.size(); i++)
-        {
-            hash[nums2[i]].first += 0;
             hash[nums2[i]].second += 1; 
-        }
+
         for(map<int, pair<int,int> >::iterator it = hash.begin(); it != hash.end(); it++)
         {
             if (it->second.first != 0 and it->second.second == 0)
