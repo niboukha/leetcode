c171d283525f53b8b30abce8c2b6c419d15a9e11
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-04-30 17:25:24 +0100

    Time: 5 ms (38.85%) | Memory: 7.9 MB (80.90%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -6,12 +6,13 @@ public:
         int         val = 0;
 
         for(int i = 0; i < s.size(); i++)
+        {
             hash[s[i] - 'a'] += 1;
+            val = max(val, hash[s[i] - 'a']);
+        }
         for (int i = 0; i < hash.size(); i++)
         {
-            if (hash[i] != 0 and val == 0)
-                val = hash[i];
-            if (val != 0 and hash[i] != 0 and val != hash[i])
+            if (hash[i] != 0 and val != hash[i])
                 return false;
         }
         return true;
