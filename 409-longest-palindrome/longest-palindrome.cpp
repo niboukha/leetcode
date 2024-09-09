2b95656893c4501adf927582973d1c26c2d7d911
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-09 10:57:37 +0100

    Time: 5 ms (31.15%) | Memory: 9.5 MB (15.76%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int longestPalindrome(string s)
     {
-        vector<int> hash(128, 0);
-        int len = 0;
+        map<char, int> hash;
+       int len = 0;
         int maxodd = 0;
 
         for (int i = 0; i < s.length(); i++)
@@ -13,20 +13,12 @@ public:
             {
                 len += hash[s[i]];
                 hash[s[i]] = 0;
-                cout << s[i] << " " << len << "\n";
             }
             else if (hash[s[i]] % 2 != 0)
-            {
                 maxodd = 1;
-                cout << "-> " << s[i] << " " << len << " " << maxodd << "\n";
-            }
         }
         if (maxodd == 1 and len < s.length())
-        {
             len += 1;
-            cout << len << " " << "dkhgel \n";
-        }
-        
         return len;    
     }
 };
