c68f82913781aafae778e6fdb89f66343f05e984
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-10 12:15:24 +0100

    Time: 19 ms (8.38%) | Memory: 16.8 MB (94.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -7,30 +7,19 @@ public:
         int m = 0;
         for (int i = 0; i < nums.size(); i++)
         {
-            // if (nums[i] <= k)
-            // {
-                hash[nums[i]] += 1;
-                m = max(m, hash[nums[i]]);
-                cout << "-----> " << nums[i] << "\n";
-            // }
+            hash[nums[i]] += 1;
+            m = max(m, hash[nums[i]]);
         }
-        // m = hash.size() + 1;
+
         vector<vector<int>> b(m + 1);
         map<int, int>::iterator it = hash.begin();
 
         for (; it != hash.end(); it++)
-        {
             b[it->second].push_back(it->first);
-            
-            cout << b[it->second][0] << " " << it->second << " " << it->first << "\n";
-            // cout << "----> " << b[it->second] << "\n";
-        }
         for (int i = b.size() - 1; i > 0; i--)
         {
-            // cout << b[1][0] << " " << i << "\n";
             if (res.size() < k)
             {
-                cout << res.size() << "\n";
                 for (int j = 0; j < b[i].size(); j++)
                   res.push_back(b[i][j]);
             }
@@ -39,9 +28,3 @@ public:
         return res;
     }
 };
-
-            // if (nums[i] <= k and l > 0 and hash[nums[i]] == 1)
-            // {
-            //     res.push_back(nums[i]);
-            //     l--;
-            // }
