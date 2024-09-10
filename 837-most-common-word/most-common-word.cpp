06e537c75cc6a4c9f5e017cf445ab3cd67dd6c34
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-10 09:20:31 +0100

    Time: 7 ms (37.50%) | Memory: 10.5 MB (70.72%) - LeetSync

--- 837-most-common-word/most-common-word.cpp
+++ 837-most-common-word/most-common-word.cpp
@@ -12,23 +12,17 @@ public:
             if (paragraph[i] != ' ' and paragraph[i] != '!' and
                 paragraph[i] != '?' and paragraph[i] != '\'' and
                 paragraph[i] != ',' and paragraph[i] != ';' and paragraph[i] != '.')
-                {
                     s += tolower(paragraph[i]);
-                    cout << s << " " << i << "\n";
-                }
             else if (!s.empty())
             {
-                cout << "---> " << s << " |" << paragraph[i] << "|\n";
                 auto it = find(banned.begin(), banned.end(), s);
                 if (it == banned.end())
                 {
-                    cout << "-----> " << s << "\n";
                     hash[s] += 1;
                     if (len <= hash[s])
                     {
                         res = s;
                         len = max(len, hash[s]);
-                        cout << "====> " << res << " " << hash[s] << " " << len << "\n";
                     }
                 }
                 s.clear();
@@ -39,13 +33,11 @@ public:
             auto it = find(banned.begin(), banned.end(), s);
             if ( it == banned.end())
             {
-                cout << "-----> t7t " << s << " " << len << "\n";
                 hash[s] += 1;
                 if (len <= hash[s])
                 {
                     res = s;
                     len = max(len, hash[s]);
-                    cout << "====> " << res << " " << hash[s] << " " << len << "\n";
                 }
             }
         }
