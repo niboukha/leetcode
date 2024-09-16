c4cad7e3258a1742ac7c3571075948bcbc6f434e
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-16 13:57:49 +0100

    Time: 4 ms (86.27%) | Memory: 14.2 MB (24.83%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int romanToInt(string s)
+    {
+        std::map<char, int> hash = {{'I', 1},
+                                    {'V', 5},
+                                    {'X', 10},
+                                    {'L', 50},
+                                    {'C', 100},
+                                    {'D', 500},
+                                    {'M', 1000}};
+        
+        int res = 0;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            if (i - 1 >= 0 and (s[i] == 'V' or s[i] == 'X') and s[i - 1] == 'I')
+            {
+                res += hash[s[i]] - 1;
+                i--;
+            }
+            else if (i - 1 >= 0 and (s[i] == 'L' or s[i] == 'C') and s[i - 1] == 'X')
+            {
+                res += hash[s[i]] - 10;
+                i--;
+            }
+            else if (i - 1 >= 0 and (s[i] == 'M' or s[i] == 'D') and s[i - 1] == 'C')
+            {
+                res += hash[s[i]] - 100;
+                i--;
+            }
+            else
+            {
+                res += hash[s[i]];
+            }
+        }
+        return res;
+    }
+};
