d98d926840bccd4f00a7f1ebc326d7e7b32c350f
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-23 14:01:27 +0100

    Time: 40 ms (90.99%) | Memory: 23.5 MB (94.61%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -5,8 +5,6 @@ public:
         for (int i = 0; i < words.size(); i++)
         {
             int left = 0, right = words[i].size() - 1;
-            if (words[i].size() == 1 or(words[i].size() == 2 and words[i][0] == words[i][1]))
-                return words[i];   
             while (left < right)
             {
                 if (words[i][left] != words[i][right] and i < words.size())
@@ -14,7 +12,6 @@ public:
                 left++;
                 right--;
             }
-            cout<< words[i] << " " << left  << " " << right << "\n";
             if (left == right or (left == right + 1 and !(words[i].size() % 2)))
                 return words[i];
         }
