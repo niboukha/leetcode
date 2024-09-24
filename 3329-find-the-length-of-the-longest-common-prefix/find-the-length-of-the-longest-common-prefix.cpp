ed91693a4e4734ada0601ea008023e3b67966142
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-24 10:22:19 +0100

    Time: 315 ms (57.22%) | Memory: 135.7 MB (84.50%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -8,13 +8,7 @@ public:
         for (int i = 0; i < arr1.size(); i++)
         {
             for (int divisor = arr1[i]; divisor >= 1; divisor /= 10)
-            {
-                // int result = arr1[i] / divisor;
-                // cout << result << " " << arr1[i] << " " << divisor << " " << i << "\n";
-                
                 hashSet.insert(divisor);
-                
-            }
         }
         
         for (int i = 0; i < arr2.size(); i++)
@@ -22,14 +16,10 @@ public:
             for (int divisor = arr2[i]; divisor >= 1; divisor /= 10)
             {
                 int result = arr2[i] / divisor;
-                // cout << result << " " << arr2[i] << " " << divisor << " " << i << "\n";
                 if (hashSet.find(divisor) != hashSet.end())
-                {
                     res = max(res, (int)to_string(divisor).size());
-                }
             }
         }
         return res;
-
     }
 };
