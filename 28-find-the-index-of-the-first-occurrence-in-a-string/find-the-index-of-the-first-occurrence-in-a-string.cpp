1e0db1b50d2e264bfcae0a65589fff3fb31fbb38
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2024-09-28 09:00:27 +0100

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.07%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -8,15 +8,11 @@ public:
         int i = 0;
         while (left < haystack.size())
         {
-            // cout << haystack[left] << "\n";
             res = right;
             while (i < needle.size())
             {
-                // cout << haystack[right] << " " << right << "\n";
                 if (haystack[right] != needle[i])
-                {
                     break;
-                }
                 else if (i == needle.size() - 1)
                     return res;
                 right++;
