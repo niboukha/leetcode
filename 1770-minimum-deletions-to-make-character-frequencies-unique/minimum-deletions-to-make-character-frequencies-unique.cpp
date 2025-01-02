f9e6444cc07ff470af1037e91103e1c18be4c1b9
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-02 10:19:47 +0100

    Time: 56 ms (14.43%) | Memory: 20.3 MB (42.68%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -4,22 +4,19 @@ public:
     {
         int res = 0;
 
-        map<char, int> freqMap;
-        set<int> freqSet;
+        map<char, int>  freqMap;
+        set<int>        freqSet;
 
         for (int i = 0; i < s.size(); i++)
             freqMap[s[i]] += 1;
         
         for (auto& pair : freqMap)
         {
-            // std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
             while (freqSet.find(pair.second) != freqSet.end())
             {
-                // cout << pair.second << "\n";
                 pair.second--;
                 res++;
             }
-            // cout << pair.second<<"---\n";
             if (pair.second != 0)
                 freqSet.insert(pair.second);
         }
