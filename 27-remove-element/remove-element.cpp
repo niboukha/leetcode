2c022789a8291fbe27a4eacb83a906ac9c6cd31f
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-04 12:08:44 +0100

    Time: 0 ms (100.00%) | Memory: 11.6 MB (46.86%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -3,52 +3,28 @@ public:
     int removeElement(vector<int>& nums, int val)
     {
         int res = 0;
-
         int left = 0;
         int right = nums.size() - 1;
-        int k = 0;
-
-        if (nums.size() == 1 and nums[0] == val)
-            return res;
 
         while(left <= right)
-        {
-            cout << nums[left] << " " << nums[right] << "\n";
-            // if (left < right and nums[left] == val and nums[right] == val)
-            // {
-            //     left++;
-            //     right--;
-            //     res++;
-            //     continue;
-            // }
-            
+        {   
             if (nums[right] == val)
             {
-                cout << nums[right] << "\n";
                 right--;
                 res++;
-                continue;
             }
-            if (nums[left] == val and nums[right] != val)
+            else if (nums[left] == val and nums[right] != val)
             {
                 swap(nums[left], nums[right]);
 
                 left++;
                 right--;
                 res++;
-                cout << res << "\n";
 
             }
             else
-            {
                 left++;
-            }
         }
-
-        for (int i = 0 ; i < nums.size(); i++)
-            cout << nums[i] << " ";
-        cout << "\n";
-        cout << res << "\n";
         return nums.size() - res;
     }
 };
