9dafa7d27fc4a30ba7eb5403e47077472933c0af
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-08 12:02:54 +0100

    Time: 0 ms (100.00%) | Memory: 9 MB (21.78%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -4,8 +4,8 @@ public:
     {
         int right = s.size() - 1;
         int left = s.size() - 1;
-        if (s.size() == 1)
-            return 1;
+        // if (s.size() == 1)
+        //     return 1;
         while(left >= 0 and s[left] == ' ')
         {
             left--;
