ee597c35b97ddf8c96f2f407dd385a7b0f7477b2
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-08 19:57:58 +0100

    Time: 0 ms (100.00%) | Memory: 12.4 MB (27.76%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -6,34 +6,21 @@ public:
         int right2 = n - 1;
         int i = n + m - 1;
 
-        // if (n == 1 and m == 0)
-        // {
-        //     nums1[0] = max(nums1[0], nums2[0]);
-        //     return;
-        // }
-        // else if (n == 0)
-        //     return;
-
         while (right2 >= 0 and right1>= 0)
         {
-            // cout << right1 << " " << right2 << " " << nums1[right1] << " " <<  nums2[right2] << " " << i << "\n";
-
             nums1[i] = max(nums1[right1], nums2[right2]);
             if (nums1[right1] >= nums2[right2])
             {
-                // cout << right1 << "\n";
                 nums1[right1] = 0;
                 right1--;
             }
             else
             {
-                cout << right2 << "-\n";
                 nums2[right2] = 0;
                 right2--;
             }    
             i--;
         }
-        // cout << i << " " << right2 << " " << right1  << " " << nums1[1]<< "\n";
         while (i >= 0 and right2 >= 0)
         {
             nums1[i] = nums2[right2];
