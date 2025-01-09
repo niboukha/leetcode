d45f47e8c3cf1bea515dad9e3e877db2ff69f480
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-09 10:56:27 +0100

    Time: 3 ms (33.60%) | Memory: 28.3 MB (20.10%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums)
+    {
+        int res = nums.size() / 2;
+        map<int, int> hash;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            hash[nums[i]] += 1;
+            res = max(res, hash[nums[i]]);
+        }
+
+        map<int, int>::iterator it = hash.begin();
+
+        while (it != hash.end()) {
+            if (it->second == res)
+                return it->first;
+            // cout << "Key: " << it->first
+            //     << ", Value: " << it->second << endl;
+            ++it;
+        }
+
+        return 0;
+    }
+};
