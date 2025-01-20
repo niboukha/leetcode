953e40d5dcc368f00750efbfcb759b3a9e101bbd
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-01-20 10:59:51 +0100

    Time: 0 ms (100.00%) | Memory: 8.3 MB (22.52%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,12 @@
+int searchInsert(int* nums, int numsSize, int target){
+    
+    for (int i =0; i < numsSize; i++)
+    {
+        if(nums[i] == target)
+            return(i);
+        else
+            if(nums[i] > target)
+                return (i);
+    }
+    return (numsSize);
+}
