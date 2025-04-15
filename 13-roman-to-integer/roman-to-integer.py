9f33abb81d21e81c3556fc0f60e1fe6e880f1de1
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-04-15 21:50:24 +0100

    Time: 2 ms (84.20%) | Memory: 17.8 MB (40.25%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hash =  {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        res = 0
+
+        for i, c in enumerate(s):
+            # print(i, hash[s[i+1]])
+            if i + 1 < len(s) and hash[s[i]] < hash[s[i + 1]]:
+                res -= hash[s[i]]
+            else:
+                res += hash[s[i]]
+
+        return res
