20d723bcc4f1b957ae297fdb986a98e474332a3a
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-04-28 22:05:12 +0100

    Time: 225 ms (7.06%) | Memory: 30.3 MB (97.85%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -5,7 +5,6 @@ class Solution:
         tar = 0
         left = right = 0
         while left < len(nums):
-            # print(left , right, tar)
             if right < len(nums) and k > ((tar + nums[right]) * (right - left + 1)):
                 tar += nums[right]
                 right += 1
