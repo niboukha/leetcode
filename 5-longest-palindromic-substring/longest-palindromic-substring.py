1ffa15d415bda6aeb8bb667b8be0f72d91738f98
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-05-08 15:16:10 +0100

    Time: 1195 ms (35.72%) | Memory: 18 MB (46.40%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,23 +1,25 @@
 class Solution:
+
+    def findSubPalindrome(self, s: str, l: int, r: int) -> str:
+
+        ans = ""
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            ans = max(ans, s[l: r+1], key=len)
+            r += 1
+            l -= 1
+        return ans
+
     def longestPalindrome(self, s: str) -> str:
         
         ans = ""
         i = l = r = 0
+        
         while i < len(s):
 
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if len(ans) < len(s[l: r+1]):
-                    ans = s[l: r+1]
-                r += 1
-                l -= 1
+            odd = self.findSubPalindrome(s, i, i)
+            even = self.findSubPalindrome(s, i, i + 1)
+            ans = max(odd, even, ans, key=len)
 
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if len(ans) < len(s[l: r+1]):
-                    ans = s[l: r+1]
-                r += 1
-                l -= 1    
             i += 1
 
         return ans
