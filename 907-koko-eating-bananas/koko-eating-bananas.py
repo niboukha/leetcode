24230f86941a011643552cbfc9a87c011e55bf0a
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-06-27 13:47:01 +0100

    Time: 450 ms (5.08%) | Memory: 19.1 MB (9.75%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -11,48 +11,15 @@ class Solution:
 
             speed = 0
             for p in piles:
-                # print("k : ", k)
                 speed += math.ceil(p / k)
 
-            # print("speed: ",speed)
             if speed <= h:
                 res = min(k, res)
                 r = k - 1
-                # print(r, k, r/2, math.ceil(r/ 2))
                 k = math.ceil(r / 2)
             else:
                 l = k + 1
-                k = int((r + l) / 2)
-                # print("up: ",l, k ,r)
+                k = math.ceil((r + l) / 2)
                 
 
         return res
-
-
-# 1 2 3 4 5 6 7 8 9 10 11
-
-# 1 2 3 4 5 6
-
-# 1 2 3 4 5
-
-# 4 5
-
-
-
-# l = 4
-# r = 5
-
-# 1 11
-
-# 1 6
-
-# 1 3
-
-
-# 3 / 4 = 0
-
-
-
-# 6 / 4 = 2
-
-# 7 / 4 = 2 
