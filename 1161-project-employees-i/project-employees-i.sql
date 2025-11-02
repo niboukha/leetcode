9472b459a7b250a74e4591177593b5b1307b05eb
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-02 19:25:22 +0100

    Time: 433 ms (82.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+
+SELECT project_id, ROUND((SUM(experience_years)::NUMERIC / COUNT(employee_id)), 2) AS average_years
+FROM Project
+LEFT JOIN Employee USING(employee_id)
+GROUP BY project_id
+ORDER BY project_id
+
