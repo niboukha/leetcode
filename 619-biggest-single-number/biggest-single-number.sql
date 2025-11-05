44c361a2468315bb152b38b814c5335b71b72217
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-05 17:11:01 +0100

    Time: 254 ms (12.00%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,14 +1,8 @@
 -- Write your PostgreSQL query statement below
 
-SELECT 
-  COALESCE(
-    (
-      SELECT num
-      FROM MyNumbers
-      GROUP BY num
-      HAVING COUNT(num) = 1
-      ORDER BY num DESC
-      LIMIT 1
-    ), 
-    NULL
-  ) AS num;
+SELECT MAX(num) as num FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
