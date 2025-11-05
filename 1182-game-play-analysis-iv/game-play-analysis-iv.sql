9a2114d373a240ce29fdb860b20f4fb9d9732c47
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-05 12:09:50 +0100

    Time: 596 ms (22.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+
+WITH Init_date AS (
+    SELECT player_id, MIN(event_date) AS event_date
+    FROM Activity
+    GROUP BY player_id
+)
+SELECT ROUND(COUNT(Init_date.event_date) * 1.0 / COUNT(DISTINCT(Activity.player_id)), 2) AS fraction
+FROM Activity
+LEFT JOIN Init_date ON
+    Activity.player_id =  Init_date.player_id
+    AND
+    Activity.event_date =  Init_date.event_date + 1
+
+
+-- smallest date then check if date day + 1 exist if it exist add 1
