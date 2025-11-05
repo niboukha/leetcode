f5c1f18125c93986d95611f2a37a1f907c669eba
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-05 12:13:33 +0100

    Time: 582 ms (24.92%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -5,12 +5,13 @@ WITH Init_date AS (
     FROM Activity
     GROUP BY player_id
 )
-SELECT ROUND(COUNT(Init_date.event_date) * 1.0 / COUNT(DISTINCT(Activity.player_id)), 2) AS fraction
+SELECT ROUND(
+        COUNT(Init_date.event_date) * 1.0 / COUNT(DISTINCT(Activity.player_id))
+    , 2) AS fraction
 FROM Activity
 LEFT JOIN Init_date ON
     Activity.player_id =  Init_date.player_id
     AND
     Activity.event_date =  Init_date.event_date + 1
 
-
--- smallest date then check if date day + 1 exist if it exist add 1
+-- smallest date then check if date (day + 1) exist
