0433caa434a59fc4ad4d2f20ca3fa16ed09fcfd4
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-06 20:43:56 +0100

    Time: 337 ms (48.24%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,12 +1,21 @@
 -- Write your PostgreSQL query statement below
 
-SELECT
-    DISTINCT(e1.reports_to) AS employee_id,
-    (SELECT name FROM Employees WHERE employee_id = e1.reports_to),
-    COUNT(e1.employee_id) AS reports_count,
-    ROUND(SUM(e1.age) * 1.0 / COUNT(e1.age)) AS average_age
+-- SELECT
+--     DISTINCT(e1.reports_to) AS employee_id,
+--     (SELECT name FROM Employees WHERE employee_id = e1.reports_to),
+--     COUNT(e1.employee_id) AS reports_count,
+--     ROUND(SUM(e1.age) * 1.0 / COUNT(e1.age)) AS average_age
+-- FROM Employees AS e1
+-- JOIN Employees AS e2 USING(reports_to)
+-- GROUP BY e2.employee_id, e1.reports_to
+
+
+SELECT 
+    e1.employee_id,
+    e1.name,
+    COUNT(*) AS reports_count,
+    ROUND(AVG(e2.age)) AS average_age
 FROM Employees AS e1
-JOIN Employees AS e2 USING(reports_to)
-GROUP BY e2.employee_id, e1.reports_to
-
-
+JOIN Employees AS e2 ON e2.reports_to = e1.employee_id
+GROUP BY e1.employee_id, e1.name
+ORDER BY e1.employee_id
