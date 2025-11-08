789bd6a4a43da721d562daee4524f56bc69e09a8
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-08 15:44:15 +0100

    Time: 487 ms (79.85%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,13 +1,16 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 
-SELECT Employee.employee_id, Employee.department_id
+SELECT employee_id, department_id
 FROM Employee
 WHERE
     primary_flag = 'Y'
-    or
-    Employee.employee_id IN (
-        SELECT employee_id
-        FROM Employee
-        GROUP BY employee_id
-        HAVING COUNT(employee_id) = 1
-    )
+
+UNION ALL
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+
+ORDER BY employee_id
+
