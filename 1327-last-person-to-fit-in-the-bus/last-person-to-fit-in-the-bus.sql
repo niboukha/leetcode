04d9428813d59a25e89b0a2a3a4555eba31a8a81
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-12 09:45:53 +0100

    Time: 480 ms (39.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,20 @@
+-- Write your PostgreSQL query statement below
+
+WITH
+    SumWeight AS (
+        SELECT
+            person_id, turn, person_name, weight,
+            CASE 
+                WHEN 
+                    SUM(weight) OVER (ORDER BY turn) <= 1000 then SUM(weight) OVER (ORDER BY turn)
+                ELSE NULL
+                
+            END
+            AS TotalWeight
+        FROM Queue
+    )
+
+SELECT person_name
+FROM SumWeight
+WHERE TotalWeight = (SELECT MAX(TotalWeight) FROM SumWeight)
+
