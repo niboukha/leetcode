ba72130e57cf69fb2b927f73b53e5db5ccf3f6f5
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-16 10:07:50 +0100

    Time: 184 ms (62.51%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -6,31 +6,12 @@ WITH
         FROM Customer
         GROUP BY visited_on
         ORDER BY visited_on
-    ),
-    sum_amounts AS (
-        SELECT
-            visited_on,
-            SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount
-        FROM group_dates
-
-    ),
-    avg_amounts AS (
-        SELECT *, ROUND(amount / 7, 2) AS average_amount
-        FROM sum_amounts
-        ORDER BY visited_on DESC
-    ),
-    ranked AS (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY visited_on DESC) AS rn
-        FROM avg_amounts
-    ),
-    count_row AS (
-        SELECT COUNT(*) - 6 AS cutoff
-        FROM ranked
     )
-    
 
-SELECT ranked.visited_on, ranked.amount, ranked.average_amount
-FROM ranked
-CROSS JOIN count_row
-WHERE rn <= count_row.cutoff
-ORDER BY ranked.visited_on ASC
+SELECT
+    visited_on,
+    SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+    ROUND(AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+FROM group_dates
+ORDER BY visited_on
+OFFSET 6 ROWS
