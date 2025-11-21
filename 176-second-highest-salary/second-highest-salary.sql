f34cc1b5457535d137b66ac9c0ae7bb692cf0e6d
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2025-11-21 11:56:30 +0100

    Time: 190 ms (96.47%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,10 +1,12 @@
 -- Write your PostgreSQL query statement below
 
-WITH rank AS (
-    SELECT *, DENSE_RANK() OVER (ORDER BY salary DESC NULLS LAST) AS rn
-    FROM Employee
-)
-select
+WITH
+    rank AS (
+        SELECT *, DENSE_RANK() OVER (ORDER BY salary DESC NULLS LAST) AS rn
+        FROM Employee
+    )
+
+SELECT
     coalesce(
         (
             SELECT DISTINCT salary
