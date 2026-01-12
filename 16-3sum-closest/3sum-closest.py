1f4f3cfc47291945d89741906d7a8cdd9948479f
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2026-01-12 09:53:26 +0100

    Time: 440 ms (10.72%) | Memory: 19.4 MB (7.92%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,17 +1,14 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        print(nums)
         n = len(nums)
         ans = nums[0] + nums[1] + nums[2]
+        nums.sort()
+        
         i = 0
-        # print(i, n-3)
         while i <= n - 3:
             l , r = i + 1, n - 1
             while l < r:
                 sum = nums[i] + nums[l] + nums[r]
-                # print(i, l, r, sum, ans)
-                # print(nums[i], nums[l], nums[r])
                 if abs(target - sum) <= abs(target - ans):
                     ans = sum
                 if sum < target:
