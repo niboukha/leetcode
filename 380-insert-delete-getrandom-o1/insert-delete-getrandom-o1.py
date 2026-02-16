ec436636f59a3eef515dfe116a9dfd9affa22acd
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2026-02-16 11:24:20 +0000

    Time: 84 ms (93.07%) | Memory: 57 MB (47.27%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,33 +1,34 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.elements = []
-        self.elementsIdx = {}
+        self.values = [] # A list to store the values
+        self.index_map = {} # A dictionary to map value -> index in the list
 
     def insert(self, val: int) -> bool:
-        if val in self.elements:
+        if val in self.index_map:
             return False
 
-        self.elementsIdx[val] = len(self.elements)
-        self.elements.append(val)
+        self.index_map[val] = len(self.values)
+        self.values.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.elements:
+        if val not in self.index_map:
             return False
 
-        idx = self.elementsIdx[val]
-        
-        self.elementsIdx[self.elements[-1]] = idx
-        del self.elementsIdx[val]
-        
-        self.elements[idx] = self.elements[-1]
-        self.elements.pop()
-        
+        idx = self.index_map[val] # Get the index of the value to remove
+        last_val = self.values[-1] # Move the last element into the removed spot
+
+        self.values[idx] = last_val
+        self.index_map[last_val] = idx
+
+        self.values.pop() # Remove the last element
+        del self.index_map[val] # Remove val from the index map
+
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.elements)
+        return random.choice(self.values)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
