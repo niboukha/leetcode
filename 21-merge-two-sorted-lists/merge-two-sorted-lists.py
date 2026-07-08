243d50448f90d13b3def3ed8e6971f5fbe3a5490
Nisrin Boukhari <73406219+niboukha@users.noreply.github.com>
2026-07-08 09:48:24 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,20 +9,9 @@ class Solution:
         if not list1 or not list2:
             return list1 if list1 else list2
         
-        dummy   = ListNode()
-        tail    = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next   = list1
-                list1       = list1.next
-            else:
-                tail.next   = list2
-                list2       = list2.next
-            tail = tail.next
-        
-        if list1:
-            tail.next = list1
-        else:
-            tail.next = list2
+        if list1.val >= list2.val:
+            list1, list2 = list2, list1
 
-        return dummy.next
+        list1.next = self.mergeTwoLists(list1.next, list2)
+
+        return list1
